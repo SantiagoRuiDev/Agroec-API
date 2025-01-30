faabfd710ed16f8a6938b61db875fe1082769e61
Santiago <117700156+SantiagoRuiDev@users.noreply.github.com>
2025-01-30 10:12:10 -0300

    v2.1 - Arreglo en tendencias de analiticas de precio de producto x zona

--- src/models/products.model.js
+++ src/models/products.model.js
@@ -2,34 +2,66 @@ import { connection } from "../index.js";
 
 export const getPriceAnalyticByProduct = async (product) => {
   try {
-    const [statement] = await connection.query(`
-      SELECT 
-          provincia, 
-          MIN(precio) AS precio_minimo,
-          MAX(precio) AS precio_maximo
-      FROM (
-      -- Precios desde producto_licitar
-      SELECT 
-          pl.precio, 
-          u.provincia
-      FROM producto_licitar pl
-      INNER JOIN usuarios u ON pl.id_usuario = u.id
-      INNER JOIN productos p ON pl.id_producto = p.id
-      WHERE p.nombre = ?
+    const [statement] = await connection.query(
+      `
+      WITH precios AS (
+        -- Seleccionamos los precios de producto_licitar
+        SELECT 
+            pl.precio, 
+            u.provincia,
+            pl.fecha_publicacion
+        FROM producto_licitar pl
+        INNER JOIN usuarios u ON pl.id_usuario = u.id
+        INNER JOIN productos p ON pl.id_producto = p.id
+        WHERE p.nombre = ?
 
-      UNION ALL
+        UNION ALL
 
-      -- Precios desde producto_vender
-      SELECT 
-          pv.precio, 
-          u.provincia
-      FROM producto_vender pv
-      INNER JOIN usuarios u ON pv.id_usuario = u.id
-      INNER JOIN productos p ON pv.id_producto = p.id
-      WHERE p.nombre = ?
-    ) AS precios_por_provincia
-    GROUP BY provincia
-`, [product, product]);
+        -- Seleccionamos los precios de producto_vender
+        SELECT 
+            pv.precio, 
+            u.provincia,
+            pv.fecha_publicacion
+        FROM producto_vender pv
+        INNER JOIN usuarios u ON pv.id_usuario = u.id
+        INNER JOIN productos p ON pv.id_producto = p.id
+        WHERE p.nombre = ?
+    ),
+    precios_ordenados AS (
+        -- Ordenamos los precios por provincia y fecha_publicacion descendente
+        SELECT 
+            provincia,
+            precio,
+            fecha_publicacion,
+            LAG(precio) OVER (PARTITION BY provincia ORDER BY fecha_publicacion DESC) AS precio_anterior
+        FROM precios
+    ),
+    precios_agrupados AS (
+        -- Calculamos el precio mínimo, máximo y el último precio registrado
+        SELECT 
+            provincia, 
+            MIN(precio) AS precio_minimo,
+            MAX(precio) AS precio_maximo,
+            FIRST_VALUE(precio) OVER (PARTITION BY provincia ORDER BY fecha_publicacion DESC) AS ultimo_precio,
+            MAX(precio_anterior) AS precio_anterior
+        FROM precios_ordenados
+        GROUP BY provincia
+    )
+    SELECT 
+        provincia, 
+        precio_minimo, 
+        precio_maximo, 
+        ultimo_precio,
+        CASE 
+            WHEN ultimo_precio >= precio_anterior THEN 'Subida'
+            WHEN ultimo_precio < precio_anterior THEN 'Bajada'
+            WHEN ultimo_precio = precio_maximo THEN 'Max'
+            ELSE 'Estable'
+        END AS tendencia
+    FROM precios_agrupados;
+`,
+      [product, product]
+    );
 
     return statement;
   } catch (error) {
