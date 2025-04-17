1f802d4077b68dc18ac4f029c73c08bf150816a8
Santiago <117700156+SantiagoRuiDev@users.noreply.github.com>
2025-04-17 18:16:02 -0300

    v2.7 - Pequeño arreglo en limite de conexiones API

--- src/database/index.js
+++ src/database/index.js
@@ -10,39 +10,9 @@ const pool = mysql.createPool({
   database: (!APP_SETTINGS.production) ? APP_SETTINGS.database_name : APP_SETTINGS.prod_database_name,
   port: (!APP_SETTINGS.production) ? 3306 : APP_SETTINGS.prod_dabatase_port,
   ssl: false,
-  connectionLimit: 5,
+  connectionLimit: 15,
   waitForConnections: true,
   queueLimit: 0
 });
 
 export default pool;
-
-// Función para verificar si la base de datos 'agroec' existe y crearla si no
-export async function checkAndCreateAgroecDatabase() {
-  try {
-    const connection = await mysql.createConnection({
-      host: (!APP_SETTINGS.production) ? APP_SETTINGS.database_host : APP_SETTINGS.prod_database_host,
-      user: (!APP_SETTINGS.production) ? APP_SETTINGS.database_user : APP_SETTINGS.prod_database_user,
-      password: (!APP_SETTINGS.production) ? APP_SETTINGS.database_password : APP_SETTINGS.prod_database_password,
-      port: (!APP_SETTINGS.production) ? 3306 : APP_SETTINGS.prod_dabatase_port,
-      ssl: false,
-      connectTimeout: 60000
-    });
-
-    const [rows] = await connection.query(`SHOW DATABASES LIKE 'agroec';`);
-
-    if (rows.length === 0) {
-      console.log('La base de datos "agroec" no existe. Creando...');
-      const sqlScript = await fs.readFile('./src/sql/default.sql', 'utf8');
-      await connection.query(sqlScript);
-      console.log('Base de datos "agroec" creada exitosamente con el archivo SQL.');
-    } else {
-      console.log('La base de datos "agroec" ya existe.');
-    }
-
-    await connection.end();
-  } catch (error) {
-    console.error('Error al verificar o crear la base de datos:', error);
-    throw error;
-  }
-}
