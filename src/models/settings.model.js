68aa52b5640fb80827c113f05c490ada947480b9
Santiago <117700156+SantiagoRuiDev@users.noreply.github.com>
2025-04-21 20:40:21 -0300

    v2.7 - Pequeño fix en configuración añadido link - soporte insumos

--- src/models/settings.model.js
+++ src/models/settings.model.js
@@ -4,8 +4,8 @@ export const updateSettings = async (schema) => {
   const db = await pool.getConnection();
   try {
     const [statement] = await db.query(
-      `UPDATE configuracion SET url_terminos_condiciones = ?, porcentaje_fee_comprador = ?, porcentaje_fee_vendedor = ?, soporte_whatsapp_uno = ?, soporte_whatsapp_dos = ?, soporte_whatsapp_tres = ?`,
-      [schema.url_terminos_condiciones, schema.porcentaje_fee_comprador, schema.porcentaje_fee_vendedor, schema.soporte_whatsapp_uno, schema.soporte_whatsapp_dos, schema.soporte_whatsapp_tres]
+      `UPDATE configuracion SET url_terminos_condiciones = ?, porcentaje_fee_comprador = ?, porcentaje_fee_vendedor = ?, soporte_whatsapp_uno = ?, soporte_whatsapp_dos = ?, soporte_whatsapp_tres = ?, url_soporte_insumos = ?`,
+      [schema.url_terminos_condiciones, schema.porcentaje_fee_comprador, schema.porcentaje_fee_vendedor, schema.soporte_whatsapp_uno, schema.soporte_whatsapp_dos, schema.soporte_whatsapp_tres, schema.url_soporte_insumos]
     );
 
     return statement.affectedRows;
